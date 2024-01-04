382d73c78467c065ca533de5302b2233100aa25b
raulrocha2 <raul.rocha2@gmail.com>
2024-01-04 18:52:54 -0300

    refactor: move business logic to a domain entity

--- src/data/usecases/facebook-authentication.ts
+++ src/data/usecases/facebook-authentication.ts
@@ -2,6 +2,7 @@ import { FacebookAuthentication } from '@/domain/features'
 import { LoadFacebookUserApi } from '@/data/contracts/apis'
 import { AuthenticationError } from '@/domain/errors'
 import { LoadUserAccountRepository, SaveFacebookAccountRepository } from '../contracts/repos'
+import { FacebookAccount } from '@/domain/models'
 
 export class FacebookAuthenticationUsecase {
   constructor (
@@ -13,13 +14,10 @@ export class FacebookAuthenticationUsecase {
     const fbData = await this.facebookApi.loadUser(params)
     if(fbData !== undefined) {
       const accountData = await this.userAccountRepo.load({ email: fbData?.email})
-      await this.userAccountRepo.saveWithFacebook({
-        id: accountData?.id,
-          name: accountData?.name ?? fbData.name,
-          email: fbData.email,
-          facebookId: fbData.facebookId
-      })
+      const fbAccount = new FacebookAccount(fbData, accountData)
+      await this.userAccountRepo.saveWithFacebook(fbAccount)
     }
     return new AuthenticationError()
   }
 }
+
---
+++ tests/domain/models/facebook-account.spec.ts
@@ -0,0 +1,36 @@
+import { FacebookAccount } from "@/domain/models"
+
+describe('FacebookAccount', () => {
+  const fbData = {
+    name: 'any_fb_name',
+    email: 'any_fb_email',
+    facebookId: 'any_fb_id'
+  }
+
+  test('should create with facebook data only', () => {
+    const sut = new FacebookAccount(fbData)
+    expect(sut).toEqual(fbData)
+  })
+
+  test('should update name if its empty', () => {
+    const acccountData = {id: 'any_id'}
+    const sut = new FacebookAccount(fbData, acccountData)
+    expect(sut).toEqual({
+      id: 'any_id',
+      name: 'any_fb_name',
+      email: 'any_fb_email',
+      facebookId: 'any_fb_id'
+    })
+  })
+
+  test('should not update name if its not empty', () => {
+    const acccountData = { id: 'any_id',  name: 'any_name' }
+    const sut = new FacebookAccount(fbData, acccountData)
+    expect(sut).toEqual({
+      id: acccountData.id,
+      name: acccountData.name,
+      email: fbData.email,
+      facebookId: fbData.facebookId
+    })
+  })
+})
