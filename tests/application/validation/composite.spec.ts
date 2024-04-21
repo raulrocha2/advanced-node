ba56902ddfc012cfad95ecdfbed3e6d0a7436ca7
raulrocha2 <raul.rocha2@gmail.com>
2024-04-20 22:03:11 -0300

    test: ensure ValidationCompostite returns the first error found

--- tests/application/validation/composite.spec.ts
+++ tests/application/validation/composite.spec.ts
@@ -1,26 +1,49 @@
-import { mock } from "jest-mock-extended";
+import { MockProxy, mock } from "jest-mock-extended";
 
 interface Validator {
   validate: () => Error | undefined;
 }
 
-class ValidationComposite {
+class ValidationComposite implements Validator {
   constructor(private readonly validators: Validator[]) {}
 
-  validate(): undefined {
-    return undefined;
+  validate(): Error | undefined {
+    for (const validator of this.validators) {
+      const error = validator.validate();
+      if (error !== undefined) {
+        return error;
+      }
+    }
   }
 }
 
 describe("ValidationComposite", () => {
-  test("should retirn undefined if all Validators return undefined", () => {
-    const validator1 = mock<Validator>();
+  let sut: ValidationComposite;
+  let validator1: MockProxy<Validator>;
+  let validator2: MockProxy<Validator>;
+  let validators: Validator[];
+
+  beforeEach(() => {
+    validator1 = mock<Validator>();
     validator1.validate.mockReturnValue(undefined);
-    const validator2 = mock<Validator>();
+    validator2 = mock<Validator>();
     validator2.validate.mockReturnValue(undefined);
-    const validators = [validator1, validator2];
-    const sut = new ValidationComposite(validators);
+    validators = [validator1, validator2];
+  });
+
+  beforeEach(() => {
+    sut = new ValidationComposite(validators);
+  });
+
+  test("should return undefined if all Validators return undefined", () => {
     const error = sut.validate();
     expect(error).toBeUndefined();
+  });
+
+  test("should return the first error", () => {
+    validator1.validate.mockReturnValue(new Error("First error"));
+    validator2.validate.mockReturnValue(new Error("Second error"));
+    const error = sut.validate();
+    expect(error).toEqual(new Error("First error"));
   });
 });
